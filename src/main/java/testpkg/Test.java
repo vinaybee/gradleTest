4aa83ebc5cb8906af9cd43941c9d5bfb7bb33aa5
Vinay Mishra <vinay@roambee.com>
2016-05-23 14:09:32 -0700

    test gradle

---
+++ src/main/java/testpkg/Test.java
@@ -0,0 +1,10 @@
+package testpkg;
+
+/**
+ * Created by admin on 5/23/16.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Testing gradle on AppAgile");
+    }
+}
